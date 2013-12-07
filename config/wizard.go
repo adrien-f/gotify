496d4833d7f9e1fe53ba5736989706ff259d1ea2
Adrien F <adrien.fillon@gmail.com>
2013-12-06 23:35:04 -0500

    Get the remember session from input

--- config/wizard.go
+++ config/wizard.go
@@ -39,8 +39,42 @@ func getPassword(configuration *Configuration) {
 	}
 }
 
+func getRemember(configuration *Configuration) {
+	for {
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("Do you want gotify to login you automatically ? [Y/n] ")
+		remember, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		if strings.TrimSpace(remember) == "" {
+			configuration.RememberMe = true
+			return
+		} else if stringInSlice(strings.TrimSpace(remember), []string{"y", "Y", "yes", "Yes"}) {
+			configuration.RememberMe = true
+			return
+		} else if stringInSlice(strings.TrimSpace(remember), []string{"n", "N", "no", "No"}) {
+			configuration.RememberMe = false
+			return
+		} else {
+			fmt.Println("I could not understand your answer, please write either yes or no")
+		}
+	}
+}
+
+// From http://stackoverflow.com/a/15323988
+func stringInSlice(a string, list []string) bool {
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
+}
+
 func StartWizard(configuration *Configuration) *Configuration {
 	getUsername(configuration)
 	getPassword(configuration)
+	getRemember(configuration)
 	return configuration
 }
