b573d5ad08cfc3a909d62aa9464bfdc4b97c452a
Adrien F <adrien.fillon@gmail.com>
2013-12-07 14:25:51 -0500

    Add some docstring and the saveConfiguration method

--- config/config.go
+++ config/config.go
@@ -14,13 +14,29 @@ type Configuration struct {
 	RememberMe bool `json:"remember_me"`
 }
 
+// Starts the wizard config
 func configWizard() *Configuration {
 	configuration := new(Configuration)
 	fmt.Println("Welcome to gotify !\nThis wizard will help you set up gotify, follow it carefully !")
 	StartWizard(configuration)
+	err := saveConfig(configuration)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return configuration
 }
 
+// Save the configuration in the config.json file
+func saveConfig(configuration *Configuration) error {
+	config, err := json.Marshal(configuration)
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile("config.json", config, 0644)
+	return err
+}
+
+// Load the configuration from config.json or launch the wizard if it does not exists
 func LoadConfig() *Configuration {
 	if _, err := os.Stat("config.json"); os.IsNotExist(err) {
 		configuration := configWizard()
